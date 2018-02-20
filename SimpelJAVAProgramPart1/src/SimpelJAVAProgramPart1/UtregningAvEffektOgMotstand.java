963ec52dd278bbf1352ce51425e6a349ce9e5611
EHHestnes <35171622+EHHestnes@users.noreply.github.com>
2018-02-20 14:31:53 +0100

    Add files via upload

---
+++ SimpelJAVAProgramPart1/src/SimpelJAVAProgramPart1/UtregningAvEffektOgMotstand.java
@@ -0,0 +1,35 @@
+﻿
+package SimpelJAVAProgramPart1;
+
+import java.util.Scanner;
+
+public class UtregningAvEffektOgMotstand {
+
+    public static void main(String[] args) {
+        double u, i, r, p; //oppretter variabler
+        Scanner inn = new Scanner(System.in); // av lesning fra tastatur
+
+        System.out.println("Dette er et simpelt regne program som regner motstand og effekt");
+        //Leser inn verdien for sturm fra tastatur
+        System.out.print("\nSkrive inn ampere/strum \t");
+        i = inn.nextDouble();
+        // Leser inn verdien for spenning fra tastatur
+        System.out.print("\nSkrive inn volt/spenningen \t");
+        u = inn.nextDouble();
+
+        //lager en if blokk som tar hensyn til hvist strumen er null
+        if (i != 0) {
+            r = u / i;
+            p = u * i;
+            System.out.println("\nmotstand\t " + r + " Ohm");
+            System.out.println("effekt\t\t " + p + " W\n");
+        } else if (i == 0) {
+            p = u * i;
+            // hvist strummen blir 0 hvil du få ikke definer pga alt du deler med null er tull 
+            System.out.println("\nmotstand\t " + "ikke definert");
+            System.out.println("effekt\t\t " + p + " W\n");
+
+        }
+    }
+
+}
